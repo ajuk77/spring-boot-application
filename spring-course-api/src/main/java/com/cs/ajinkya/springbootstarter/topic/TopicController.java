e8b699b0f3a9bde6c23439127f82ff8dd8537ffc
ajinkya.kulkarni <ajinkya.kulkarni77@gmail.com>
2017-10-29 13:24:32 -0700

    Added Topic API and Topic POJO Mapper

--- spring-course-api/src/main/java/com/cs/ajinkya/springbootstarter/topic/TopicController.java
+++ spring-course-api/src/main/java/com/cs/ajinkya/springbootstarter/topic/TopicController.java
@@ -1,14 +1,20 @@
 package com.cs.ajinkya.springbootstarter.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-	
+
 	@RequestMapping("/topics")
-	public String getAllTopics() {
-		return "All Topics";
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("spring", "Spring Framework", "Spring Framework Description"),
+				new Topic("hibernate", "Hibernate Framework", "Hibernate Framework Description")
+		);
 	}
 
 }
